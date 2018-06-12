4b6d111718e9ee8431f444554cf41e6e7653c4c2
DKondoff <danielkondov91gmail.com>
2018-06-12 15:39:49 +0300

    HW

--- 01. IntroToNodeJS/into/index.js
+++ 01. IntroToNodeJS/into/index.js
@@ -9,3 +9,25 @@ storage.save()
 storage.clear()
 
 storage.load()
+
+storage.saveAsync(() => {
+  storage.clear()
+
+  storage.loadAsync(() => {
+    let afterLoadValue = storage.get('test')
+    console.log(afterLoadValue)
+  })
+})
+
+storage
+  .saveAsPromis()
+  .then(() => {
+    storage.clear()
+
+    storage
+      .loadAsPromis()
+      .then(() => {
+        let afterLoadValue = storage.get('test')
+        console.log(afterLoadValue)
+      })
+  })
--- 01. IntroToNodeJS/into/storage.js
+++ 01. IntroToNodeJS/into/storage.js
@@ -47,10 +47,65 @@ let save = () => {
   let dataAsString = JSON.stringify(data)
   fs.writeFileSync(dataFile, dataAsString)
 }
+
+let saveAsync = (callback) => {
+  let dataAsString = JSON.stringify(data)
+  fs.writeFile(dataFile, dataAsString, (err) => {
+    if (err) {
+      console.log(err)
+      return
+    }
+
+    callback()
+  })
+}
+
+let saveAsPromis = () => {
+  return new Promise((resolve, reject) => {
+    let dataAsString = JSON.stringify(data)
+    fs.watchFile(dataFile, dataAsString, err => {
+      if (err) {
+        reject(err)
+        return
+      }
+
+      resolve()
+    })
+  })
+}
+
 let load = () => {
   let dataAsString = fs.readFileSync(dataFile, 'utf8')
   data = JSON.parse(dataAsString)
 }
+
+let loadAsync = (callback) => {
+  fs.readFile(dataFile, 'utf8', (err, dataJson) => {
+    if (err) {
+      console.log(err)
+    }
+
+    data = JSON.parse(dataJson)
+    callback()
+  })
+}
+
+let loadAsPromis = () => {
+
+  return new Promise((resolve, reject) => {
+    fs.readFile(dataFile, 'utf8', (err, dataJson) => {
+      if (err) {
+        reject(err)
+        return
+      }
+
+      data = JSON.parse(dataJson)
+      resolve()
+    })
+  })
+}
+
+
 module.exports = {
   put: put,
   get: get,
@@ -58,5 +113,9 @@ module.exports = {
   update: update,
   clear: clear,
   save: save,
-  load: load
+  load: load,
+  saveAsync: saveAsync,
+  loadAsync: loadAsync,
+  loadAsPromis: loadAsPromis,
+  saveAsPromis: saveAsPromis
 }
